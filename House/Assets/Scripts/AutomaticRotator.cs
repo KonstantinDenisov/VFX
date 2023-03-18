9806fb505cf999a14b339fd5c5d0cdade8513027
KonstantinDenisov <omacs@mail.ru>
2023-03-18 18:23:56 +0300

    Refactoring AutomaticRotator by code style

--- House/Assets/Scripts/AutomaticRotator.cs
+++ House/Assets/Scripts/AutomaticRotator.cs
@@ -6,17 +6,15 @@ public class AutomaticRotator : MonoBehaviour
     [SerializeField] private float _speedRotation;
     [SerializeField] private float _timeCycle;
     [SerializeField] private Transform _transform;
-    private Vector3 _vector3;
 
     public void StartDOTween()
     {
-        _vector3 = new Vector3(0, _speedRotation, 0);
-        _transform.DORotate(_vector3, _timeCycle , RotateMode.LocalAxisAdd).SetLoops (-1, LoopType.Restart);
+        _transform.DORotate(new Vector3(0, _speedRotation, 0), _timeCycle , RotateMode.LocalAxisAdd)
+            .SetLoops (-1, LoopType.Restart);
     }
 
     public void StopDOTween()
     {
         _transform.DOKill();
     }
-    
 }
