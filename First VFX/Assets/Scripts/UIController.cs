a47a75d8dc8a75cf4a0e6eb543d361519e4ed1a8
KonstantinDenisov <omacs@mail.ru>
2023-04-05 20:52:44 +0300

    Create heel VFX

---
+++ First VFX/Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Button _play;
+    [SerializeField] private Animator _animator;
+    
+    private void Awake()
+    {
+        _play.onClick.AddListener(PlayAnimation);
+    }
+
+    private void PlayAnimation()
+    {
+        _animator.SetTrigger("Spell");
+    }
+}
