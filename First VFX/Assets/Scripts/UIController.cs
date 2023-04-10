bb9593e62efbc9909bb1153eb63cb1d10dcb97d2
KonstantinDenisov <omacs@mail.ru>
2023-04-11 00:16:01 +0300

    Create light in the hands

--- First VFX/Assets/Scripts/UIController.cs
+++ First VFX/Assets/Scripts/UIController.cs
@@ -6,6 +6,8 @@ public class UIController : MonoBehaviour
     [SerializeField] private Button _play;
     [SerializeField] private Animator _animator;
     [SerializeField] private ParticleSystem _spell;
+    [SerializeField] private ParticleSystem _rightHand;
+    [SerializeField] private ParticleSystem _leftHand;
     
     private void Awake()
     {
@@ -16,5 +18,7 @@ public class UIController : MonoBehaviour
     {
         _animator.SetTrigger("Spell");
         _spell.Play();
+        _rightHand.Play();
+        _leftHand.Play();
     }
 }
