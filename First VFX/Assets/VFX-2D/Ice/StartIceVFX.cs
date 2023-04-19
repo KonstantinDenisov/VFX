92f35c9811a842eb6bec9070e3b480ab03dfadda
KonstantinDenisov <omacs@mail.ru>
2023-04-19 15:16:04 +0300

    Correction freezing effect

--- First VFX/Assets/VFX-2D/Ice/StartIceVFX.cs
+++ First VFX/Assets/VFX-2D/Ice/StartIceVFX.cs
@@ -7,7 +7,7 @@ namespace VFX_2D.Ice
 {
     public class StartIceVFX : MonoBehaviour
     {
-        [SerializeField] private float _appearanceTime = 1f;
+        [SerializeField] private float _appearanceTime = 0.7f;
         [SerializeField] private SpriteRenderer _spriteRenderer;
         [SerializeField] private Transform _transform;
         [SerializeField] private Button _button;
@@ -20,8 +20,17 @@ namespace VFX_2D.Ice
 
         private void PlayerVFX()
         {
-            _spriteRenderer.DOFade(1, _appearanceTime);
-            _particleSystem.Play();
+            _particleSystem.Stop();
+            _particleSystem.Clear();
+            
+            _spriteRenderer.DOFade(0f, 0f);
+            _spriteRenderer.transform.localScale = Vector3.one * 1.5f;
+
+            DOTween.Sequence()
+                .AppendCallback(_particleSystem.Play)
+                .Append(_spriteRenderer.DOFade(1f, _appearanceTime).SetEase(Ease.InSine))
+                .Join(_spriteRenderer.transform.DOScale(Vector3.one * 2, 0.6f).SetEase(Ease.OutBack))
+                .SetDelay(0.4f);
         }
     }
 }
