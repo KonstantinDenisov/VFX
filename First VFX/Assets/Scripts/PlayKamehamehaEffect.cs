ad20d459cf594a604852679d7b76fff36a39f45a
KonstantinDenisov <omacs@mail.ru>
2023-04-24 20:17:30 +0300

    Create animation

---
+++ First VFX/Assets/Scripts/PlayKamehamehaEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayKamehamehaEffect : MonoBehaviour
+{
+    [SerializeField] private Button _play;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private ParticleSystem _spell;
+    [SerializeField] private ParticleSystem _rightHand;
+    [SerializeField] private ParticleSystem _leftHand;
+    
+    private void Awake()
+    {
+        _play.onClick.AddListener(PlayAnimation);
+    }
+
+    private void PlayAnimation()
+    {
+        _animator.SetTrigger("Spell");
+       // _spell.Play();
+       // _rightHand.Play();
+        //_leftHand.Play();
+    }
+}
